0bee6527518e5ee4f7315cd755e621d815c5955e
Aleksey Verin <verevaa@yandex.ru>
2022-12-24 10:24:22 +0300

    удалил старые файлы

--- aleksey-verin/09 Weather plus ASYNC-AWAIT/README.md
+++
@@ -1,2 +0,0 @@
-# weather
-https://aleksey-verin.github.io/weather/
--- aleksey-verin/09 Weather plus ASYNC-AWAIT/test.js
+++
@@ -1,7 +0,0 @@
-let newSet = new Set()
-newSet.add('Omsk')
-newSet.add('Baku')
-newSet.add('Tomsk')
-newSet.add('Peru')
-
-console.log(newSet)
