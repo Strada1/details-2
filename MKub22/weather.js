7c0007d3526faed16dd38822a8487f5bb403ae58
Kuba <kubm2016@gmail.com>
2022-12-24 17:38:39 +0300

    more modules

--- MKub22/weather.js
+++ MKub22/weather.js
@@ -2,7 +2,7 @@
 
 import { ELEMENTS } from "./uiElements.js";
 import { TABS } from "./uiElements.js";
-
+import { checkClickTabs, changeTabs} from "./changingTabs.js";
 
 
 TABS.weatherButtons.addEventListener("click", function (event) {
@@ -11,24 +11,6 @@ TABS.weatherButtons.addEventListener("click", function (event) {
 		changeTabs(clickButton)
 	}
 });
-
-function checkClickTabs(clickButton) {
-	const clickButtonParent = clickButton.closest(".weather__button")
-	const activeClickElementParent = clickButtonParent.classList.contains("-active")
-	return clickButtonParent && !activeClickElementParent
-}
-
-function changeTabs(clickButton) {
-	const activeButton = TABS.weatherButtons.querySelector('.weather__button.-active');
-	activeButton.classList.remove("-active")
-	clickButton.classList.add("-active")
-	const indexClickButton = TABS.weatherButtonAll.findIndex(item => item === clickButton)
-	const activeTab = TABS.weatherTabs.querySelector('.weather__tab.-active');
-	activeTab.classList.remove("-active");
-	const newActiveTab = TABS.weatherTabAll[indexClickButton];
-	newActiveTab.classList.add("-active");
-}
-
 
 let storage = [];
 
