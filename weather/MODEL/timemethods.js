429064e5fbcb72c02f9e0ab77fd277adb5abb04a
darknes7 <40527801+iskander11@users.noreply.github.com>
2023-01-09 05:59:44 +0300

    Delete weather directory

--- weather/MODEL/timemethods.js
+++
@@ -1,7 +0,0 @@
-const TIMEMETHOD = {
-  TIMESTAMP_TO_HOURS_MINUTES: 'timestamp to hours and minutes',
-  DT_TO_DAY_MONTH: 'date and time to day and month',
-  DT_TO_HOURS_MINUTES: 'date and time to hours and minutes',
-};
-
-export default TIMEMETHOD;
